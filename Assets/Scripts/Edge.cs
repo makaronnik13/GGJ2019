47ed5b5a4b62e7afdee105614cc5d4759524b709
Nikita Tishkin <13nik@mail.ru>
2019-01-26 13:36:08 +0300

    EdgePrefabs

---
+++ Assets/Scripts/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour
+{
+    public SpriteRenderer WhiteEdge;
+    private Color _transparent = new Color(1,1,1,0);
+
+    private float MaxDist = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(Glow.Instance.transform.position, transform.position);
+
+        dist = Mathf.Min(dist, MaxDist) / MaxDist;
+
+        WhiteEdge.color = Color.Lerp(Color.white, _transparent, dist);
+    }
+}
