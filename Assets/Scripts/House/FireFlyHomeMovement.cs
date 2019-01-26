7ae897589694e7368d5d6991dc84a780aea5caab
Fedor Vishnyakov <wishnyakow@gmail.com>
2019-01-26 16:43:34 +0300

    BugHouse init commit

---
+++ Assets/Scripts/House/FireFlyHomeMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FireFlyHomeMovement : MonoBehaviour
+{
+	public float MovementSpeed, RotationSpeed;
+	private Vector3 DirectionPoint;
+
+	void Start()
+	{
+		DirectionPoint = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//transform.Translate(transform.up*Time.deltaTime*MovementSpeed);
+		if (Input.GetMouseButtonDown(0))
+		{
+			DirectionPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+		Vector2 direction = new Vector2(DirectionPoint.x - transform.position.x, DirectionPoint.y - transform.position.y);
+		transform.position += Time.deltaTime * MovementSpeed * transform.up;
+		transform.up = Vector2.Lerp(transform.up, direction, Time.deltaTime * RotationSpeed);
+	}
+}
---
+++ Assets/Scripts/House/HatchScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatchScript : MonoBehaviour
+{
+	public Transform RotationPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
